608a360fcce3aee1c1998defecb6b1aedba73e35
jenishan1611 <jenishan1611@gmail.com>
2023-11-14 21:08:02 +0530

    test

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-docker build -t reactimg .
-docker-compose down || true
-docker-compose up -d
+# Stop and remove existing containers (if any)
+docker-compose down  || true
 
+# Build the Docker image
+docker build -t reactimg .
+
+# Start the containers in detached mode
+docker-compose up -d
